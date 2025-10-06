6d8dd8ed97726a55e536437e9a3753363eb1b790
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-07 00:02:52 +0600

    updated for drizzle kit

---
+++ src/database/url.entity.ts
@@ -0,0 +1,5 @@
+// url.entity.ts
+// import { sql } from 'drizzle-orm';
+// import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+// doc: https://trilon.io/blog/nestjs-drizzleorm-a-great-match
