eeec30a774bbdb7b0cf65055ba15bc0266da0a1a
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-07 11:24:20 +0600

    chore: updated auto openapi doc generation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 
 import { NestFactory } from '@nestjs/core';
@@ -17,6 +19,10 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
+
+  app.getHttpAdapter().get('/openapi-json', (req, res) => {
+    res.json(document);
+  });
 
   app.use(
     '/scaler',
